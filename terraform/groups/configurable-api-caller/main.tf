5ea41d24f7cf4bdea3dacced3e89d11a813cf1fb
DrewHarvard <aharvard@companieshouse.gov.uk>
2021-07-15 10:55:51 +0100

    Rework efs changes and use terraform-runner
    Removes the count from efs resources as not required.
    Updates to dissolution to match existing state.
    
    Removed local terraform and common script so we can use terraform-runner which is the approach we currently take.
    Had to copy state to new location prior to running changes.

--- terraform/groups/configurable-api-caller/main.tf
+++ terraform/groups/configurable-api-caller/main.tf
@@ -79,7 +79,7 @@ resource "aws_iam_role_policy_attachment" "param_read" {
 resource "aws_cloudwatch_event_rule" "call_api_caller_lambda" {
   name                = "call_api_caller_lambda"
   description         = "Cloudwatch event to call ${aws_lambda_function.configurable_api_lambda.function_name} lambda routinely"
-  schedule_expression = "rate(1 hour)"
+  schedule_expression = "rate(5 minutes)"
 }
 
 resource "aws_cloudwatch_event_target" "event_target_api_caller" {
@@ -141,27 +141,24 @@ resource "aws_lambda_permission" "allow_cloudwatch_dissolutions_rebel1" {
 }
 
 resource "aws_cloudwatch_event_rule" "call_api_caller_lambda_efs_handle_delayed_submission_sameday" {
-  count               = var.deploy_to == "development" ? 1 : 0
   name                = "call_api_caller_lambda_efs_handle_delayed_submission_sameday"
   description         = "Cloudwatch event to call ${aws_lambda_function.configurable_api_lambda.function_name} lambda routinely, which calls EFS API to check for any delayed same day submissions"
   schedule_expression = "cron(0/15 7-17 ? * MON-FRI *)"
 }
 
 resource "aws_cloudwatch_event_target" "event_target_api_caller_efs_handle_delayed_submission_sameday" {
-  count     = var.deploy_to == "development" ? 1 : 0
-  target_id = aws_cloudwatch_event_rule.call_api_caller_lambda_efs_handle_delayed_submission_sameday[0].id
-  rule      = aws_cloudwatch_event_rule.call_api_caller_lambda_efs_handle_delayed_submission_sameday[0].name
+  target_id = aws_cloudwatch_event_rule.call_api_caller_lambda_efs_handle_delayed_submission_sameday.id
+  rule      = aws_cloudwatch_event_rule.call_api_caller_lambda_efs_handle_delayed_submission_sameday.name
   arn       = aws_lambda_function.configurable_api_lambda.arn
   input     = file("profiles/${var.aws_profile}/efs_handle_delayed_submission_sameday.json")
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_efs_handle_delayed_submission_sameday" {
-  count         = var.deploy_to == "development" ? 1 : 0
   statement_id  = "AllowExecutionFromCloudWatchEFS"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.configurable_api_lambda.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.call_api_caller_lambda_efs_handle_delayed_submission_sameday[0].arn
+  source_arn    = aws_cloudwatch_event_rule.call_api_caller_lambda_efs_handle_delayed_submission_sameday.arn
 }
 
 // VPC
