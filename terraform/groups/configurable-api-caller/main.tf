2a2c58afb76f463e2656cbee94989210dde39bae
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2024-06-18 10:16:08 +0100

    BI-13242 removed reference to Aard1

--- terraform/groups/configurable-api-caller/main.tf
+++ terraform/groups/configurable-api-caller/main.tf
@@ -280,34 +280,6 @@ resource "aws_lambda_permission" "allow_cloudwatch_account_validator_cleanup_sub
   source_arn    = aws_cloudwatch_event_rule.call_api_caller_lambda_account_validator_cleanup_submissions[0].arn
 }
 
-//
-// Should only be development
-//
-resource "aws_cloudwatch_event_rule" "call_api_caller_lambda_account_validator_cleanup_submissions_aard1" {
-  count               = var.deploy_to == "development" ? 1 : 0 
-  name                = "call_api_caller_lambda_account_validator_cleanup_submissions_aard1"
-  description         = "Cloudwatch event to call ${aws_lambda_function.configurable_api_lambda.function_name} lambda at 2am everyday"
-  schedule_expression = "cron(0 2 * * ? *)"
-}
-
-resource "aws_cloudwatch_event_target" "event_target_api_caller_account_validator_cleanup_submissions_aard1" {
-  count     = var.deploy_to == "development" ? 1 : 0
-  target_id = aws_cloudwatch_event_rule.call_api_caller_lambda_account_validator_cleanup_submissions_aard1[0].id
-  rule      = aws_cloudwatch_event_rule.call_api_caller_lambda_account_validator_cleanup_submissions_aard1[0].name
-  arn       = aws_lambda_function.configurable_api_lambda.arn
-  input     = file("profiles/${var.aws_profile}/common-${var.aws_region}/account_validator_cleanup_submissions_aard1.json")
-}
-
-resource "aws_lambda_permission" "allow_cloudwatch_account_validator_cleanup_submissions_aard1" {
-  count         = var.deploy_to == "development" ? 1 : 0
-  statement_id  = "AllowExecutionFromCloudWatchAccountValidatorCleanupSubmissionsAard1"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.configurable_api_lambda.function_name
-  principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.call_api_caller_lambda_account_validator_cleanup_submissions_aard1[0].arn
-}
-
-
 // VPC
 
 data "terraform_remote_state" "applications_vpc" {
