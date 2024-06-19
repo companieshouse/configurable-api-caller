9a78eb8b7892bf67228cbaaa3b6ec2c83890a767
okoye-companieshouse <obilikha@companieshouse.gov.uk>
2024-06-19 10:06:50 +0100

    BI-13242 update terraform

--- terraform/groups/configurable-api-caller/main.tf
+++ terraform/groups/configurable-api-caller/main.tf
@@ -257,27 +257,24 @@ resource "aws_lambda_permission" "allow_cloudwatch_efs_submit_files_to_fes" {
 // Future work required to double check staging and live profiles are still correct, when count is changed.
 //
 resource "aws_cloudwatch_event_rule" "call_api_caller_lambda_account_validator_cleanup_submissions" {
-  count               = var.deploy_to == "development" ? 1 : 0 
   name                = "call_api_caller_lambda_account_validator_cleanup_submissions"
   description         = "Cloudwatch event to call ${aws_lambda_function.configurable_api_lambda.function_name} lambda at 2am everyday"
   schedule_expression = "cron(0 2 * * ? *)"
 }
 
 resource "aws_cloudwatch_event_target" "event_target_api_caller_account_validator_cleanup_submissions" {
-  count     = 1
-  target_id = aws_cloudwatch_event_rule.call_api_caller_lambda_account_validator_cleanup_submissions[0].id
-  rule      = aws_cloudwatch_event_rule.call_api_caller_lambda_account_validator_cleanup_submissions[0].name
+  target_id = aws_cloudwatch_event_rule.call_api_caller_lambda_account_validator_cleanup_submissions.id
+  rule      = aws_cloudwatch_event_rule.call_api_caller_lambda_account_validator_cleanup_submissions.name
   arn       = aws_lambda_function.configurable_api_lambda.arn
   input     = file("profiles/${var.aws_profile}/common-${var.aws_region}/account_validator_cleanup_submissions.json")
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_account_validator_cleanup_submissions" {
-  count         = 1
   statement_id  = "AllowExecutionFromCloudWatchAccountValidatorCleanupSubmissions"
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.configurable_api_lambda.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.call_api_caller_lambda_account_validator_cleanup_submissions[0].arn
+  source_arn    = aws_cloudwatch_event_rule.call_api_caller_lambda_account_validator_cleanup_submissions.arn
 }
 
 // VPC
