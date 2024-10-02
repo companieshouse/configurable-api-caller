06e785309e775532a8b9c62f5c804ba48d788b29
Ivana Pavic <ipavic@companieshouse.gov.uk>
2024-10-02 18:48:51 +0100

    updating provider block

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -4,6 +4,17 @@ provider "aws" {
 
 terraform {
   backend "s3" {}
+  required_version = "~> 1.3"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.54.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 3.18.0"
+    }
+  }
 }
 
 // Lambda
