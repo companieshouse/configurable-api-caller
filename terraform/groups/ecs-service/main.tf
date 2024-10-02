100e7da803464d39771f1e1e3e642065b7c27234
Ivana Pavic <ipavic@companieshouse.gov.uk>
2024-10-02 22:43:49 +0100

    updating provider version

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.54.0"
+      version = "~> 5.68.0"
     }
     vault = {
       source  = "hashicorp/vault"
