89dbdedb1c123e6f00db4bf68860799c5612cd96
wdhda-ch <127737882+wdhda-ch@users.noreply.github.com>
2024-11-04 07:39:30 +0000

    Add count to call_api_caller_lambda_account_validator_cleanup_submissions

--- terraform/groups/configurable-api-caller/main.tf
+++ terraform/groups/configurable-api-caller/main.tf
@@ -281,6 +281,7 @@ resource "aws_lambda_permission" "allow_cloudwatch_efs_submit_files_to_fes" {
 // Future work required to double check staging and live profiles are still correct, when count is changed.
 //
 resource "aws_cloudwatch_event_rule" "call_api_caller_lambda_account_validator_cleanup_submissions" {
+  count               = var.deploy_to == "development" ? 1 : 0 
   name                = "call_api_caller_lambda_account_validator_cleanup_submissions"
   description         = "Cloudwatch event to call ${aws_lambda_function.configurable_api_lambda.function_name} lambda at 2am everyday"
   schedule_expression = "cron(0 2 * * ? *)"
