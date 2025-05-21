ee1ee7355db565491a740f8edf24c49b8d6471c6
bcullerton <bcullerton@companieshouse.gov.uk>
2025-05-21 16:48:13 +0100

    Fixes mismatch between aws plugin and tf version

--- terraform/groups/configurable-api-caller/main.tf
+++ terraform/groups/configurable-api-caller/main.tf
@@ -4,6 +4,13 @@ provider "aws" {
 
 terraform {
   backend "s3" {}
+  required_version = ">= 0.12, < 1.3"
+
+  required_providers {
+    aws = {
+      version = ">= 5.0, < 6.0"
+    }
+  }
 }
 
 // Lambda
