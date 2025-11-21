d0b9877e96eb740a4c7e5938e1e7f56d8727995a
pshdev0 <pshdev0@gmail.com>
2025-11-21 06:55:39 +0000

    refactor data

--- terraform/groups/configurable-api-caller/data.tf
+++ terraform/groups/configurable-api-caller/data.tf
@@ -39,41 +39,41 @@ data "aws_iam_policy_document" "get_param_store_systems_manager_policy" {
 }
 
 data "local_file" "input" {
-  filename = "profiles/${var.aws_account}/common-${var.aws_region}/input.json"
+  filename = "${local.json_folder}/input.json"
 }
 
 data "local_file" "dissolutions_submit" {
-  filename = "profiles/${var.aws_account}/common-${var.aws_region}/dissolutions_submit.json"
+  filename = "${local.json_folder}/dissolutions_submit.json"
 }
 
 data "local_file" "efs_handle_delayed_submission_sameday" {
-  filename = "profiles/${var.aws_account}/common-${var.aws_region}/efs_handle_delayed_submission_sameday.json"
+  filename = "${local.json_folder}/efs_handle_delayed_submission_sameday.json"
 }
 
 data "local_file" "efs_queue_files" {
-  filename = "profiles/${var.aws_account}/common-${var.aws_region}/efs_queue_files.json"
+  filename = "${local.json_folder}/efs_queue_files.json"
 }
 
 data "local_file" "efs_submit_files_to_fes" {
-  filename = "profiles/${var.aws_account}/common-${var.aws_region}/efs_submit_files_to_fes.json"
+  filename = "${local.json_folder}/efs_submit_files_to_fes.json"
 }
 
 data "local_file" "process_pending_refunds" {
-  filename = "profiles/${var.aws_account}/common-${var.aws_region}/process_pending_refunds.json"
+  filename = "${local.json_folder}/process_pending_refunds.json"
 }
 
 data "local_file" "payments_status_check" {
-  filename = "profiles/${var.aws_account}/common-${var.aws_region}/payments_status_check.json"
+  filename = "${local.json_folder}/payments_status_check.json"
 }
 
 data "local_file" "account_validator_cleanup_submissions" {
-  filename = "profiles/${var.aws_account}/common-${var.aws_region}/account_validator_cleanup_submissions.json"
+  filename = "${local.json_folder}/account_validator_cleanup_submissions.json"
 }
 
 data "local_file" "dissolutions_submit_rebel1" {
-  filename = "profiles/${var.aws_account}/common-${var.aws_region}/dissolutions_submit_rebel1.json"
+  filename = "${local.json_folder}/dissolutions_submit_rebel1.json"
 }
 
 data "local_file" "dissolutions_submit_phoenix1" {
-  filename = "profiles/${var.aws_account}/common-${var.aws_region}/dissolutions_submit_phoenix1.json"
+  filename = "${local.json_folder}/dissolutions_submit_phoenix1.json"
 }
